bf22c5b2207d21f43248af4faac76fd9bde7659c
Pavel <mankov.174@gmail.com>
2024-12-25 21:52:20 +0500

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void testPostmanEcho() {
+
+    }
+}
